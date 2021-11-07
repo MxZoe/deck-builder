14a7a50ecc6382bfb5d98ad6d15ec8243b17e51e
Zoe Weinstein <mxzweinstein@gmail.com>
2021-11-07 16:36:33 -0700

    gave up on modal and copy button for this session

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+/* function is depreciated and needs updating 11/7/21
+function copyText(id){
+  $(id).select();
+  document.execCommand('copy');
+}
+*/
+
 $(document).ready(function(){
 const cards = [];
 const sideCards = ["SIDEBOARD"];
@@ -20,4 +27,11 @@ const sideCards = ["SIDEBOARD"];
     event.preventDefault();
   });
 
+  /*copy button does not work as of 11/7/21
+  $("#copyButton").click(function(event){
+    alert("Your deck has been copied to your clipboard.");
+    copyText("'#mainDisplay'")
+    event.preventDefault();
+  });
+  */
 });
